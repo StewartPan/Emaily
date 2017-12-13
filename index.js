67b10f8f1d9f536f3fb94a0dd33ade8cf8b96d0d
admin <admin@nwk-32-12.nwk.ruw.rutgers.edu>
2017-12-13 13:21:51 -0500

    Connect to mongoDB and set cookie

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
-app.get('/', (req, res) => {
-	res.send({hi: 'there'});
-});
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+const app = express();
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes')(app);
+
+// clientID 
+// clientSecret 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
