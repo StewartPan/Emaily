a000a7ff329c593f7e87e7591acabe250238b8da
admin <admin@nwk-35-152.nwk.ruw.rutgers.edu>
2018-01-07 17:58:28 -0500

    fix passport.js typo

--- services/passport.js
+++ services/passport.js
@@ -13,21 +13,23 @@ passport.deserializeUser((id, done) => {
 		done(null, user);
 	});
 });
-passport.use(new GoogleStrategy({
-		clientID: keys.googleClientID,
-		clientSecret: keys.googleClientSecret,
-		callbackURL: '/auth/google/callback',
-		proxy: true
-	}, 
-	    async (accessToken, refreshToken, profile, done) => {
-	      const existingUser = await User.findOne({ googleId: profile.id });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-	      if (existingUser) {
-	        return done(null, existingUser); //don't forget the return statement
-	      }
+      if (existingUser) {
+        return done(null, existingUser); //don't forget the return statement
+      }
 
-	      const user = await new User({ googleId: profile.id }).save();
-	      done(null, user);
-	    }
-	)
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
 );
